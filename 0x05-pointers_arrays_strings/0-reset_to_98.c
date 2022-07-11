15ed64dbcc74bb5590b12530cc968095de1ea4a0
Yelbeneh Abayneh Assefa <yelbenehaby@gmail.com>
2022-07-11 02:01:34 -0700

    4. I can only go one way.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - check the code for Holberton School students.
+ * reset_to_98 -  takes a pointer to an int as parameter and updates the value it points to to 98.
  * @n: is a pointer int type
  * Return: Always 0.
  */
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: pointer int type
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; len++)
+		s++;
+
+	s--;
+	for (len = len; len != 0; len--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
